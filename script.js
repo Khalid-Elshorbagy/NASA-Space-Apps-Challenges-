fcdfd3ac16d81a127fe76030837857a2dc342cef
Khalid-Elshorbagy <474319688@gharbeia2.moe.edu.eg>
2025-09-02 21:02:59 +0300

    save

---
+++ script.js
@@ -0,0 +1,7 @@
+// Check if JavaScript is working
+console.log("JavaScript is connected ✅");
+
+// Function to change text
+function changeText() {
+    document.getElementById("demo").innerText = "The text has changed with JavaScript 🎉";
+}
